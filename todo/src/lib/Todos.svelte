76edb4b470116210f59fa1f0c211ec6d32752364
VeQox <sonnleitner.luki@gmail.com>
2022-11-13 18:06:11 +0100

    add simple text length check in the ui

--- todo/src/lib/Todos.svelte
+++ todo/src/lib/Todos.svelte
@@ -2,7 +2,7 @@
     import { Todos, loadTodos, addTodoItem, deleteTodoItem, updateTodoItemStatus, updateTodoItemText } from "../stores/todostore"
     import { onMount } from "svelte";
 
-    let newTodoText : string;
+    let newTodoText : string = "";
 
     const addTodo = async() => {
         console.log(await addTodoItem(newTodoText))
@@ -23,11 +23,20 @@
     }) 
 </script>
 
-<form on:submit|preventDefault={() => {
-    addTodo()
-}}>
-    <input bind:value={newTodoText} placeholder="todo text" class="text-2xl w-full placeholder-neutral-500 bg-neutral-900 border-2 border-t-0 border-neutral-800 focus:border-neutral-700 rounded-b-md outline-none text-center"/>
-</form>
+<div class="grid grid-cols-6 md:grid-cols-12">
+    <div class="col-span-5 md:col-span-11">
+        <form on:submit|preventDefault={() => {
+            addTodo()
+        }}>
+            <input bind:value={newTodoText} placeholder="todo text" class="text-2xl w-full placeholder-neutral-500 bg-neutral-900 border-2 border-t-0 border-neutral-800 focus:border-neutral-700 rounded-bl-md outline-none text-center"/>
+        </form>
+    </div>
+    <div class="{newTodoText.length > 280 ? "text-red-800" : newTodoText.length > 230 ? "text-orange-800" : "text-green-800"} flex items-center justify-center bg-neutral-900 border-b-2 border-r-2 border-neutral-800 focus:border-neutral-700 rounded-br-md">
+        <p>{newTodoText.length}/280</p>
+    </div>
+
+</div>
+
 
 <div class="mt-1">
 {#each $Todos as todoItem, i}
@@ -41,7 +50,7 @@
                     <input bind:value={todoItem.text} class="w-full { todoItem.completed ? "text-neutral-500" : "" } pl-2 placeholder-neutral-500 bg-neutral-900 border border-neutral-800 focus:border-neutral-700 rounded-md outline-none">
                 </form>
             </div>
-            <div class="flex justify-center items-center">
+            <div class="flex justify-center items-center ">
                 <button class="h-6 w-6 border rounded-full border-neutral-700 hover:bg-red-800" on:click={() => removeTodoFromList(i)}></button>
             </div>
         </div>
