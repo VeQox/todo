8449c24dfd46f0b195ec6050688c769fe70c0620
VeQox <sonnleitner.luki@gmail.com>
2022-11-18 15:10:02 +0100

    add ss actions for login and register

---
+++ todo/src/routes/auth/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    let viewLogIn : boolean = true;
+</script>
+
+<div class="w-screen h-screen flex justify-center items-center">
+    <div class="grid grid-cols-2 w-1/2">
+        <button class="h-10 { viewLogIn ? "bg-neutral-800 hover:bg-neutral-700" : "bg-green-800 hover:bg-green-700" } rounded-tl-md"
+        on:click={() => viewLogIn = false }
+        >
+            Sign Up
+        </button>
+
+        <button class="h-10 { viewLogIn ? "bg-green-800 hover:bg-green-700" : "bg-neutral-800 hover:bg-neutral-700" } rounded-tr-md"
+        on:click={() => viewLogIn = true }
+        >
+            Log In
+        </button>
+
+        {#if viewLogIn}
+        <div class="col-span-2 flex justify-center items-center h-48 border-2 border-t-0 border-neutral-800 rounded-b-md"> 
+            <form class="w-10/12 space-y-4" method="post" action="?/login">
+                <input name="email" autocomplete="email" type="email" placeholder="email" class="w-full placeholder-neutral-500 bg-neutral-900 border border-neutral-800 focus:border-neutral-700 rounded-md outline-none text-center">
+                <input name="password" autocomplete="current-password" type="password" placeholder="password" class="w-full placeholder-neutral-500 bg-neutral-900 border border-neutral-800 focus:border-neutral-700 rounded-md outline-none text-center">
+                <button type="submit" class="w-full bg-green-800 hover:bg-green-700 rounded-md outline-none text-center">{viewLogIn ? "Log In" : "Sign Up"}</button>
+            </form>
+        </div>
+        {:else}
+        <div class="col-span-2 flex justify-center items-center h-48 border-2 border-t-0 border-neutral-800 rounded-b-md"> 
+            <form class="w-10/12 space-y-4" method="post"  action="?/register">
+                <input name="email" autocomplete="email" type="email" placeholder="email" class="w-full placeholder-neutral-500 bg-neutral-900 border border-neutral-800 focus:border-neutral-700 rounded-md outline-none text-center">
+                <input name="password" autocomplete="current-password" type="password" placeholder="password" class="w-full placeholder-neutral-500 bg-neutral-900 border border-neutral-800 focus:border-neutral-700 rounded-md outline-none text-center">
+                <button type="submit" class="w-full bg-green-800 hover:bg-green-700 rounded-md outline-none text-center">{viewLogIn ? "Log In" : "Sign Up"}</button>
+            </form>
+        </div>
+        {/if}
+    </div>
+</div>
