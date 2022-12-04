0f8efabb3b125c8db59f77c01b27352200ffccf4
VeQox <sonnleitner.luki@gmail.com>
2022-12-04 18:23:43 +0100

    format

--- todo/src/routes/todos/+page.svelte
+++ todo/src/routes/todos/+page.svelte
@@ -3,12 +3,12 @@
   import { onMount } from "svelte";
   import { loadTodos, Todos } from "../../stores/todostore";
   import type { ActionData } from "./$types";
- 
-  export let data : {
-    data: any[] | undefined,
+
+  export let data: {
+    data: any[] | undefined;
   };
 
-  onMount(async() => {
+  onMount(async () => {
     await loadTodos(data.data);
   });
 
@@ -18,13 +18,20 @@
     style: "short", // other values: "short" or "narrow"
   });
 
-  const getDateFormated = (date : string) => {
-    return dateFormatter.format(daysBetween(new Date(Date.now()), new Date(date)), "days")
-  }
+  const getDateFormated = (date: string) => {
+    return dateFormatter.format(
+      daysBetween(new Date(Date.now()), new Date(date)),
+      "days",
+    );
+  };
 
-  function daysBetween(first : Date, second : Date) {
+  function daysBetween(first: Date, second: Date) {
     let one = new Date(first.getFullYear(), first.getMonth(), first.getDate());
-    let two = new Date(second.getFullYear(), second.getMonth(), second.getDate());
+    let two = new Date(
+      second.getFullYear(),
+      second.getMonth(),
+      second.getDate(),
+    );
 
     let millisecondsPerDay = 1000 * 60 * 60 * 24;
     let millisBetween = two.getTime() - one.getTime();
@@ -32,45 +39,70 @@
 
     return Math.floor(days);
   }
-  
+
   //export let form : ActionData;
-  let todoTitle : string = "";
-  let todoDescription : string = "";
+  let todoTitle: string = "";
+  let todoDescription: string = "";
   $: todoTitleCharCount = todoTitle.length;
   $: todoDescriptionCharCount = todoDescription.length;
 </script>
 
-<form action="?/add" method="post" use:enhance={({form, data, action, cancel}) => {
-        return async ({ result, update }) => {
-          if(result.type === "success"){
-            todoTitle = "";
-            todoDescription = "";
-          }
-        };
-    }}>
+<form
+  action="?/add"
+  method="post"
+  use:enhance={({ form, data, action, cancel }) => {
+    return async ({ result, update }) => {
+      if (result.type === "success") {
+        todoTitle = "";
+        todoDescription = "";
+      }
+    };
+  }}
+>
   <div class="grid grid-cols-6 md:grid-cols-12">
     <div class="col-span-5 md:col-span-11">
-        <input bind:value={todoTitle} name="title" placeholder="todo text" class="text-2xl w-full placeholder-neutral-500 bg-neutral-900 border-2 border-t-0 border-neutral-800 focus:border-neutral-700 rounded-bl-md outline-none text-center"/>
+      <input
+        bind:value={todoTitle}
+        name="title"
+        placeholder="todo text"
+        class="text-2xl w-full placeholder-neutral-500 bg-neutral-900 border-2 border-t-0 border-neutral-800 focus:border-neutral-700 rounded-bl-md outline-none text-center"
+      />
     </div>
-    <div class="{todoTitleCharCount > 100 ? "text-red-800" : todoTitleCharCount > 80 ? "text-orange-800" : "text-green-800"} flex items-center justify-center bg-neutral-900 border-b-2 border-r-2 border-neutral-800 focus:border-neutral-700 rounded-br-md">
+    <div
+      class="{todoTitleCharCount > 100
+        ? 'text-red-800'
+        : todoTitleCharCount > 80
+        ? 'text-orange-800'
+        : 'text-green-800'} flex items-center justify-center bg-neutral-900 border-b-2 border-r-2 border-neutral-800 focus:border-neutral-700 rounded-br-md"
+    >
       <p>{todoTitleCharCount}</p>
     </div>
 
     <div class="col-span-5 md:col-span-11">
-      <input bind:value={todoDescription} name="description" placeholder="todo description" class="text-md w-full placeholder-neutral-500 bg-neutral-900 border-2 border-t-0 border-neutral-800 focus:border-neutral-700 rounded-bl-md outline-none text-center"/>
+      <input
+        bind:value={todoDescription}
+        name="description"
+        placeholder="todo description"
+        class="text-md w-full placeholder-neutral-500 bg-neutral-900 border-2 border-t-0 border-neutral-800 focus:border-neutral-700 rounded-bl-md outline-none text-center"
+      />
     </div>
-    <div class="{todoDescriptionCharCount > 250 ? "text-red-800" : todoDescriptionCharCount > 200 ? "text-orange-800" : "text-green-800"} flex items-center justify-center bg-neutral-900 border-b-2 border-r-2 border-neutral-800 focus:border-neutral-700 rounded-br-md">
+    <div
+      class="{todoDescriptionCharCount > 250
+        ? 'text-red-800'
+        : todoDescriptionCharCount > 200
+        ? 'text-orange-800'
+        : 'text-green-800'} flex items-center justify-center bg-neutral-900 border-b-2 border-r-2 border-neutral-800 focus:border-neutral-700 rounded-br-md"
+    >
       <p>{todoDescriptionCharCount}</p>
     </div>
-  </div>  
-  <button type="submit"></button>
+  </div>
+  <button type="submit" />
 </form>
-
 
 <div class="w-full flex justify-center">
   <div class="w-11/12 text-center space-y-3">
     {#each $Todos as todo, i}
-    <!--
+      <!--
       <div class="col-span-11 flex justify-center">
         <form method="post" action="?/update&id={todo.id}" use:enhance={({form, data, action, cancel}) => {
             return async ({ result, update }) => {
@@ -93,32 +125,61 @@
         </form>
       </div>
       -->
-        <!-- svelte-ignore a11y-click-events-have-key-events -->
-        <div class="w-full grid grid-cols-12 border border-neutral-800 rounded-md hover:cursor-pointer" on:click={() => alert("div")}>
-          <div class="flex justify-center items-center">
-            <form method="post" action="?/completed&id={todo.id}" use:enhance={({form, data, action, cancel}) => {
-              return async ({ result, update }) => {
-              };
-            }}>
-              <input name="completed" readonly bind:value={todo.completed} class="hidden">
-              <button type="submit" class="rounded-full {todo.completed ? "bg-green-800" : "border-2 border-neutral-800"}  w-4 h-4" on:click={(event) => event.stopPropagation()}></button>
-            </form>
-          </div>
-          <div class="col-span-10">
-            <p name="title" class="w-full border-b border-neutral-800">{todo.title}</p>
-          </div>
-          <div class="">
-            <form method="post" action="?/remove&id={todo.id}" use:enhance on:click={(event) => event.stopPropagation()}>
-              <button type="submit">X</button>
-            <form/>
-          </div>
-          <div class="flex justify-center items-center">
-            <p class="text-xs text-neutral-400">{getDateFormated(todo.deadline)}</p>
-          </div>
-          <div class="col-span-11">
-            <p name="title" class="w-full text-sm">{todo.description}</p>
-          </div>
+      <!-- svelte-ignore a11y-click-events-have-key-events -->
+      <div
+        class="w-full grid grid-cols-12 border border-neutral-800 rounded-md hover:cursor-pointer"
+        on:click={() => alert("div")}
+      >
+        <div class="flex justify-center items-center">
+          <form
+            method="post"
+            action="?/completed&id={todo.id}"
+            use:enhance={({ form, data, action, cancel }) => {
+              return async ({ result, update }) => {};
+            }}
+          >
+            <input
+              name="completed"
+              readonly
+              bind:value={todo.completed}
+              class="hidden"
+            />
+            <button
+              type="submit"
+              class="rounded-full {todo.completed
+                ? 'bg-green-800'
+                : 'border-2 border-neutral-800'}  w-4 h-4"
+              on:click={(event) => event.stopPropagation()}
+            />
+          </form>
         </div>
+        <div class="col-span-10">
+          <p class="w-full border-b border-neutral-800">
+            {todo.title}
+          </p>
+        </div>
+        <div class="">
+          <form
+            method="post"
+            action="?/remove&id={todo.id}"
+            use:enhance
+            on:click={(event) => event.stopPropagation()}
+          >
+            <button type="submit">X</button>
+            <form />
+          </form>
+        </div>
+        <div class="flex justify-center items-center">
+          <p class="text-xs text-neutral-400">
+            {getDateFormated(todo.deadline)}
+          </p>
+        </div>
+        <div class="col-span-11">
+          <p class="w-full text-sm text-start">
+            {todo.description}
+          </p>
+        </div>
+      </div>
     {/each}
   </div>
 </div>
