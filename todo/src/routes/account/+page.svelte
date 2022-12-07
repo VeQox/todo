bd58bcd9873c869dfc6cf43f56db12ad7c284ab7
VeQox <sonnleitner.luki@gmail.com>
2022-12-07 22:24:07 +0100

    format documents and add use:enhance to forms

--- todo/src/routes/account/+page.svelte
+++ todo/src/routes/account/+page.svelte
@@ -1,12 +1,26 @@
 <script lang="ts">
-    import {page} from "$app/stores"
+  import { enhance } from "$app/forms";
+  import { page } from "$app/stores";
 
-    let email = $page.data.session?.user.email
+  let email = $page.data.session?.user.email;
 </script>
 
 <div class="w-screen h-screen flex justify-center items-center">
-    <form method="post" action="/auth?/logout" class="w-1/2 space-y-4">
-        <input bind:value={email} type="email" readonly class="w-full placeholder-neutral-500 bg-neutral-900 border border-neutral-800 focus:border-neutral-700 rounded-md outline-none text-center">
-        <button class="w-full bg-green-800 hover:bg-green-700 rounded-md outline-none text-center">Logout</button>
-    </form>
+  <form
+    method="post"
+    action="/auth?/logout"
+    class="w-1/2 space-y-4"
+    use:enhance
+  >
+    <input
+      bind:value={email}
+      type="email"
+      readonly
+      class="w-full placeholder-neutral-500 bg-neutral-900 border border-neutral-800 focus:border-neutral-700 rounded-md outline-none text-center"
+    />
+    <button
+      class="w-full bg-green-800 hover:bg-green-700 rounded-md outline-none text-center"
+      >Logout</button
+    >
+  </form>
 </div>
